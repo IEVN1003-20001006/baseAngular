166ec0bd433743ae99ffb4b5f06b06277947d946
Jazmin Ramirez <ramirez12corona@gmail.com>
2024-09-20 20:25:52 -0600

    20-09-2024 Ejemplo1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBaseComponent } from './formularios/operas-base/operas-base.component';
+import { Ejemplo1Component } from './formularios/ejemplo1/ejemplo1.component';
+import { CineComponent } from './formularios/cine/cine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBaseComponent
+    OperasBaseComponent,
+    Ejemplo1Component,
+    CineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
